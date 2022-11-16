5146bce1507edc7631bd837d736a9d90311a5d4c
Chufretalas <marcobene.contact@gmail.com>
2022-11-16 18:47:13 -0300

    finished the basic structure and styling of the home page

---
+++ src/lib/components/HomeColumn.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	export let columnTitle: string;
+</script>
+
+<div class="column">
+	<h2 class="column-title">{columnTitle}</h2>
+	<hr />
+	<ul class="column-list">
+		<slot />
+	</ul>
+</div>
+
+<style>
+	.column {
+		display: grid;
+		grid-template-rows: 1fr 0.2fr 20fr;
+		justify-content: center;
+		width: 30%;
+		background-color: var(--secondary-color-darker);
+		border-radius: 20px;
+		border: 2px solid var(--accent-color);
+		padding: 10px;
+		box-sizing: border-box;
+	}
+
+	hr {
+		border: 0;
+		clear: both;
+		display: block;
+		width: 96%;
+		background-color: var(--accent-color);
+		height: 1px;
+	}
+
+	.column-title {
+		text-align: center;
+		font-size: 1.5rem;
+		font-weight: 800;
+	}
+
+	.column-list {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		max-height: 65vh;
+		width: 18vw;
+		overflow-x: hidden;
+		overflow-y: auto;
+        row-gap: 10px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import Footer from "$components/Footer.svelte";
 	import Navbar from "$components/Navbar.svelte";
+	import HomeColumn from "$components/HomeColumn.svelte";
+
+	const exampleData = [
+		"Something 1",
+		"Something 2",
+		"Something 3",
+		"Something 4",
+		"Something 5",
+		"Something 6",
+		"Something 7",
+		"Something 8"
+	];
 </script>
 
 <svelte:head>
@@ -10,39 +22,39 @@
 <div class="content-wrapper">
 	<Navbar />
 	<main>
-		<div class="main-content-wrapper">
-			<div class="column followers-wrapper">
-				<h2 class="column-title">Follower</h2>
-				<ul class="followers-list">
-					<li>Someone</li>
-					<li>Someone else</li>
-				</ul>
-			</div>
-			<div class="column repos-wrapper">
-				<h2 class="column-title">Repositories</h2>
-				<ul class="repos-list">
-					<li>Something</li>
-					<li>Something else</li>
-				</ul>
-			</div>
-			<div>
-				<h2 class="column column-title">Starred</h2>
-				<ul class="starred-list">
-					<li>Something</li>
-					<li>Something else</li>
-				</ul>
-			</div>
-		</div>
+		<HomeColumn columnTitle="Followers">
+			{#each exampleData as data (data)}
+				<li>{data} | followers</li>
+			{/each}
+		</HomeColumn>
+		<HomeColumn columnTitle="Repos">
+			{#each exampleData as data (data)}
+				<li>{data} | repos</li>
+			{/each}
+		</HomeColumn>
+		<HomeColumn columnTitle="Starred">
+			{#each exampleData as data (data)}
+				<li>{data} | starred</li>
+			{/each}
+		</HomeColumn>
 	</main>
 	<Footer />
 </div>
 
 <style>
+	main {
+		margin-top: 20px;
+		justify-self: center;
+		height: 100%;
+		width: 80%;
+		display: flex;
+		justify-content: space-evenly;
+	}
 	.content-wrapper {
 		height: 100%;
 		width: 100%;
 		display: grid;
-		grid-template-rows: 1fr 12fr 1fr;
+		grid-template-rows: 2fr 14fr 1fr;
 		row-gap: 10px;
 		align-items: center;
 	}
