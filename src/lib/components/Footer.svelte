975de3d5f914d8fa625d26a701fa71684dd8e7fc
Chufretalas <marcobene.contact@gmail.com>
2022-11-16 18:05:11 -0300

    did the footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer>
+	<span>
+		Made by <a
+			href="https://github.com/Chufretalas/svelte_github_search"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			Chufretalas
+		</a> - 2022
+	</span>
+</footer>
+
+<style>
+    footer {
+        justify-self: center;
+        align-self: flex-end;
+        height: 50%;
+        width: 30%;
+        background-color: var(--primary-color-darker);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top-right-radius: 10px;
+        border-top-left-radius: 10px;
+		box-shadow: 0px 0px 8px var(--blue-color);
+    }
+
+    footer span {
+        margin: auto;
+    }
+</style>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,81 @@
+<nav>
+	<div class="active-user-wrapper">
+		<img src="https://github.com/Chufretalas.png" alt="" class="active-user-avatar" />
+		<span class="active-user-nickname">Chufretalas</span>
+	</div>
+	<form action="" class="search-form">
+		<input
+			type="text"
+			name="nickname"
+			id="nickname-input"
+			placeholder="Type a user nickname here"
+		/>
+		<button class="search-button">Search</button>
+	</form>
+</nav>
+
+<style>
+    nav {
+		position: sticky;
+		top: 0;
+		height: 12vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		column-gap: 40px;
+		padding: 0 20vw;
+		background-color: var(--primary-color-darker);
+		box-shadow: 0px 0px 15px black;
+	}
+
+	.active-user-wrapper {
+		display: inherit;
+		align-items: center;
+		column-gap: 10px;
+	}
+
+	.active-user-wrapper img {
+		height: 10vh;
+		border-radius: 1000px;
+	}
+
+	.active-user-wrapper span {
+		font-size: 1.5rem;
+		font-weight: 800;
+	}
+
+	.search-form {
+		display: inherit;
+		align-items: center;
+		justify-content: center;
+		column-gap: 5px;
+	}
+
+	.search-form input {
+		border-radius: 10px;
+		border: 3px solid var(--blue-color);
+		height: 6vh;
+		padding: 3px 5px;
+		font-size: 1.2rem;
+	}
+
+	.search-form input::placeholder {
+		font-size: 0.9rem;
+	}
+
+	.search-form button {
+		border-radius: 10px;
+		border: 3px solid var(--accent-color);
+		height: 6vh;
+		padding: 2px 5px;
+		background-color: var(--secondary-color-darker);
+		color: var(--text-color);
+		font-size: 1.2rem;
+		transition: all 200ms;
+	}
+
+	.search-form button:hover {
+		cursor: pointer;
+		height: 7vh;
+	}
+</style>
